00f26da7574a8e409e83a613153a66adf01c5522
nakai <takayuki.nakai@nijibox.co.jp>
2018-05-02 17:38:19 +0900

    add: sample.html / todo.js comment: 公式の「はじめに」を完了

--- app.js
+++ app.js
@@ -18,3 +18,37 @@ const app3 = new Vue({
     seen: true
   }
 })
+
+const app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      {text: 'walk'},
+      {text: 'run'},
+      {text: 'stop'},
+    ],
+    dones: {
+      date: 'yesterday',
+      cost: 800
+    }
+  }
+})
+
+const app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: '！たけやぶやけた？'
+  },
+  methods: {
+    reverseMessage(){
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+const app6 = new Vue({
+  el: '#app-6',
+  data: {
+    favorite: ''
+  },
+})
